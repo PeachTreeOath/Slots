5ed9d83b3869c07082525db7f63b0240453cc8ff
PeachTreeOath <peachtreeoath@gmail.com>
2017-09-22 11:44:54 -0600

    Initial commit

---
+++ Assets/PeachLib/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public static class VectorExtensions {
+
+    public static Quaternion GetRotationAngleTowardsTarget(this Vector3 self, Vector3 target)
+    {
+        Vector3 vectorToTarget = self- target;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        return q;
+    }
+
+    public static Vector2 Rotate(this Vector2 v, float degrees)
+    {
+        return Quaternion.Euler(0, 0, degrees) * v;
+    }
+}
+
+public static class TransformDeepChildExtension
+{
+    //Breadth-first search
+    public static Transform FindDeepChild(this Transform aParent, string aName)
+    {
+        var result = aParent.Find(aName);
+        if (result != null)
+            return result;
+        foreach (Transform child in aParent)
+        {
+            result = child.FindDeepChild(aName);
+            if (result != null)
+                return result;
+        }
+        return null;
+    }
+}
